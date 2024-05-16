69ae0f2cfb15b3e929de3d56f4fb336202117202
Andrew Diaz <andrewdiazn20@gmail.com>
2024-05-15 17:28:58 -0700

    route-for-places

--- controllers/places.js
+++ controllers/places.js
@@ -1,13 +1,31 @@
 const router = require('express').Router()
+const places = require('../modules/places.js')
 
 router.get('/', (req, res) => {
     let places = []
-    res.render('places/index', {places})
+    res.render('places/index', { places })
 })
 
 router.get('/new', (req, res) => {
     res.render('places/new')
-  })
-  
+})
+
+router.post('/', (req, res) => {
+    console.log(req.body)
+    if (!req.body.pic) {
+        // Default image if one is not provided
+        req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+        req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+        req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
+})
+
+
 
 export default router
