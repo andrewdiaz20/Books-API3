e92ed1310e9faca3bdb1e87a829b8a0efd94ebc4
Andrew Diaz <andrewdiazn20@gmail.com>
2024-05-17 13:57:33 -0700

    edit-route-finished

---
+++ views/edit.jsx
@@ -0,0 +1,19 @@
+const React = require('react')
+const Def = require('../default.jsx')
+
+function edit_form(data){
+    return(
+        <form method="POST" action={`/places/${data.place.id}?_method=PUT`}>
+  <div className="form-group">
+      <label htmlFor="name">Place Name</label>
+      <input 
+        className="form-control" 
+        id="name" 
+        name="name" 
+        value={data.place.name} 
+      required />
+  </div>
+</form>
+
+    )
+}
